fbfe8901790e8e523ce7be955c53c9f6d411a802
Kreisler Ramirez <itskreisler@outlook.com>
2023-03-09 14:31:04 -0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes } from 'react-router-dom'
+import { HashRouter as Router, Routes } from 'react-router-dom'
 import { TagAppContext } from './context/AppContext'
 import appPages from './pages/Paths'
 import TagMenu from './components/TagMenu'
--- src/components/TagListMenu.jsx
+++ src/components/TagListMenu.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import { appPages } from '../pages/Paths'
-import { useLocation, Link, useNavigate } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import { Nav } from 'react-bootstrap'
 const TagListMenu = () => {
   const { pathname } = useLocation()
-  const navigateTo = useNavigate()
-
   return appPages.map(({ url, title }, index) => {
-    const handleCLickLinkTo = () => navigateTo(url)
     return (
       !url.includes(':') && !url.includes('dashboard') && (
-          <Nav.Link active={pathname === url} key={index} href={'#' + url} onClick={handleCLickLinkTo}>
+          <Nav.Link active={pathname === url} key={index} href={'#' + url}>
             <span className="h5 text-decoration-none">{title}</span>
           </Nav.Link>
       )
